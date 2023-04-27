089ac98a0b26930da9b8e53eb2017865d5d81409
iamseye <iamseye@gmail.com>
2023-04-27 12:31:20 -0400

    init commit

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-3 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+      <div>@Jessie Huang 2023</div>
+    </footer>
+  );
+};
+export default Footer;
